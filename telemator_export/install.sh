47695ed25af28561b41e3a25c60a1a89cf307403
Bård Schjander Flugon <bflugon@gmail.com>
2017-07-12 12:40:02 +0200

    Changed some packages to be installed by apt

--- telemator_export/install.sh
+++ telemator_export/install.sh
@@ -3,6 +3,6 @@
 # To be used on distros based on Debian
 # Must be run as root
 
-apt-get install freetds-dev
+apt-get -y --no-install-recommends install freetds-dev python3 python3-pip python3-sqlalchemy python3-psycopg2 python3-pandas
 
 pip3 install -r requirements.txt
