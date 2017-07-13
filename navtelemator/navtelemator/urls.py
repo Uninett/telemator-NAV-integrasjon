a905583611eb9b35835113224f0047f3f67e7297
Bård Schjander Flugon <bflugon@gmail.com>
2017-07-13 13:45:49 +0200

    Moved all urls to the same scope

--- navtelemator/navtelemator/urls.py
+++ navtelemator/navtelemator/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from views import circuit
 
 urlpatterns = patterns('',
-    url(r'^telemator/circuits/', circuit.render_circuits, name='circuit-list'),
-    url(r'^telemator/circuits/(?P<circuitid>.+)', circuit.render_circuit, name='circuit-info'),
-    url(r'^(?P<roomid>.+)/circuits/', circuit.room_circuits, name='room-info-circuits'),
+    url(r'^/circuits/', circuit.render_circuits, name='circuit-list'),
+    url(r'^/circuits/(?P<circuitid>.+)', circuit.render_circuit, name='circuit-info'),
+    url(r'^rooms/(?P<roomid>.+)/circuits/', circuit.room_circuits, name='room-info-circuits'),
 )
