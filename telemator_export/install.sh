71a392a15b50d6abbdae0e22177b30ed29cc32be
Bård Schjander Flugon <bflugon@gmail.com>
2017-08-24 12:47:53 +0200

    Changed pandas from debian package to pip
    This was done because the debian package is extremely outdated.
    It results in a very lengthy build process, so it should be changed back when debian has updated the package

--- telemator_export/install.sh
+++ telemator_export/install.sh
@@ -8,7 +8,7 @@ cp local_urls.py /etc/nav/python/
 cp local_settings.py /etc/nav/python/
 chown -R nav:nav /etc/nav/python
 
-apt-get -y --no-install-recommends install freetds-dev python3 python3-pip python3-sqlalchemy python3-psycopg2 python3-pandas
+apt-get -y --no-install-recommends install freetds-dev python3 python3-pip python3-sqlalchemy python3-psycopg2
 
 pip3 install -r requirements.txt
 pip install dist/*
