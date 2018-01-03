566528ceed512389cafda6df73e655f2a18613aa
Bård S. Flugon <bflugon@gmail.com>
2018-01-03 12:53:22 +0100

    Fixed using wrong fields in templates

--- navtelemator/urls.py
+++ navtelemator/urls.py
@@ -3,8 +3,8 @@ from views import circuit, owner, cable, customer
 
 urlpatterns = patterns('',
                        url(r'^cables/(?P<cableid>.+)', cable.render_cable, name='cable-info'),
-                       url(r'^circuits/(?P<circuitid>.+)', circuit.render_circuit, name='circuit-info'),
                        url(r'^circuits/', circuit.render_circuits, name='circuit-list'),
+                       url(r'^circuits/(?P<circuitid>.+)', circuit.render_circuit, name='circuit-info'),
                        url(r'^customers/(?P<customerid>.+)', customer.render_customer, name='customer-info'),
                        url(r'^ipdevinfo/(?P<netbox_sysname>.+)/circuits/', circuit.netbox_circuits,
                            name='netbox-info-circuits'),
