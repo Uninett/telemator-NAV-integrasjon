bbb4febc5b1884fbedd83166ff33c22e50d0554f
Bård S. Flugon <bflugon@gmail.com>
2018-01-04 11:51:33 +0100

    Added basic logging

--- navtelemator/services.py
+++ navtelemator/services.py
@@ -2,12 +2,15 @@ from sqlalchemy import create_engine, or_
 from sqlalchemy.orm import sessionmaker
 from navtelemator.models import Cable, Circuit, Connection, Customer, Owner, Port, RoutingCable
 from django.conf import settings
+import logging
 
 TM_USER = getattr(settings, "TM_USER", None)
 TM_PASSWORD = getattr(settings, "TM_PASSWORD", None)
 TM_HOST = getattr(settings, "TM_HOST", None)
 TM_PORT = getattr(settings, "TM_PORT", '1433')
 TM_DBNAME = getattr(settings, "TM_DBNAME", None)
+
+logger = logging.getLogger(__name__)
 
 # Get parameters from the config, used for connecting to the server
 db_params = 'mssql+pymssql://' + TM_USER + ':' + TM_PASSWORD + '@' + TM_HOST + ':' + TM_PORT + '/' + TM_DBNAME
@@ -19,56 +22,92 @@ session = Session()
 
 
 def get_cable_by_id(cable):
-    return session.query(Cable).filter(Cable.Cable == cable).one()
+    logger.info('get_cable_by_id gave length called')
+    result = session.query(Cable).filter(Cable.Cable == cable).one()
+    logger.info('get_cable_by_id gave length: %d', len(result))
+    return result
 
 def get_cables_by_end(end):
-    return session.query(Cable).filter(or_(Cable.End_A == end, Cable.End_B == end)).all()
+    logger.info('get_cables_by_end called')
+    result = session.query(Cable).filter(or_(Cable.End_A == end, Cable.End_B == end)).all()
+    logger.info('get_cables_by_end gave length: %d', len(result))
+    return result
 
 
 def get_circuit_by_id(circuit):
-    return session.query(Circuit).filter(Circuit.Circuit == circuit.upper()).one()
+    logger.info('get_circuit_by_id called')
+    result = session.query(Circuit).filter(Circuit.Circuit == circuit.upper()).one()
+    logger.info('get_circuit_by_id gave length: %d', len(result))
+    return result
 
 
 def get_circuit_details():
-    return session.query(Connection, Port).filter(Connection.End == Port.End, Connection.Card == Port.Card,
+    logger.info('get_circuit_details called')
+    result = session.query(Connection, Port).filter(Connection.End == Port.End, Connection.Card == Port.Card,
                                                   Connection.Port == Port.Port)
+    logger.info('get_circuit_details gave length: %d', len(result))
+    return result
 
 
 def get_circuit_details_by_netbox(end):
-    return session.query(Connection, Port).filter(Connection.End == end, Connection.Wire == 'A')\
+    logger.info('get_circuit_details_by_netbox called')
+    result = session.query(Connection, Port).filter(Connection.End == end, Connection.Wire == 'A')\
         .filter(Connection.End == Port.End, Connection.Card == Port.Card,Connection.Port == Port.Port).all()
+    logger.info('get_circuit_details_by_netbox gave length: %d', len(result))
+    return result
 
 
 def get_circuit_details_by_room(room):
-    return session.query(RoutingCable, Cable).filter(or_(Cable.End_A == room, Cable.End_B == room))\
+    logger.info('get_circuit_details_by_room called')
+    result = session.query(RoutingCable, Cable).filter(or_(Cable.End_A == room, Cable.End_B == room))\
         .filter(RoutingCable.Cable == Cable.Cable, RoutingCable.Wire == 'A').all()
+    logger.info('get_circuit_details_by_room gave length: %d', len(result))
+    return result
 
 
 def get_circuits():
-    return session.query(Circuit).all()
+    logger.info('get_circuits called')
+    result = session.query(Circuit).all()
+    logger.info('get_circuits gave length: %d', len(result))
+    return result
 
 def get_circuits_by_end(end):
     pass
 
 
 def get_customer_by_id(custid):
-    return session.query(Customer).filter(Customer.CustId == custid).one()
+    logger.info('get_customer_by_id called')
+    result = session.query(Customer).filter(Customer.CustId == custid).one()
+    logger.info('get_customer_by_id gave length: %d', len(result))
+    return result
 
 
 # def get_customer_by_name(name):
 #   return session.query(Customer).filter(Customer.CustId == name).all()
 
 def get_connections_by_circuit(circuit):
-    return session.query(Connection).filter(Connection.Circuit == circuit, Connection.Wire == 'A')
+    logger.info('get_connections_by_circuit called')
+    result = session.query(Connection).filter(Connection.Circuit == circuit, Connection.Wire == 'A')
+    logger.info('get_connections_by_circuit gave length: %d', len(result))
+    return result
 
 
 def get_owner_by_id(owner):
-    return session.query(Owner).filter(Owner.Owner == owner).one()
+    logger.info('get_owner_by_id called')
+    result = session.query(Owner).filter(Owner.Owner == owner).one()
+    logger.info('get_owner_by_id gave length: %d', len(result))
+    return result
 
 
 def get_routingcables_by_cable(cable):
-    return session.query(RoutingCable).filter(RoutingCable.Cable == cable, RoutingCable.Wire == 'A').all()
+    logger.info('get_routingcables_by_cable called')
+    result = session.query(RoutingCable).filter(RoutingCable.Cable == cable, RoutingCable.Wire == 'A').all()
+    logger.info('get_routingcables_by_cable gave length: %d', len(result))
+    return result
 
 
 def get_routingcables_by_circuit(circuit):
-    return session.query(RoutingCable).filter(RoutingCable.Circuit == circuit, RoutingCable.Wire == 'A').all()
+    logger.info('get_routingcables_by_circuit called')
+    result = session.query(RoutingCable).filter(RoutingCable.Circuit == circuit, RoutingCable.Wire == 'A').all()
+    logger.info('get_routingcables_by_circuit gave length: %d', len(result))
+    return result
