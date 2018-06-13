c657d47745919c5e6bf7e72eb1b852ba83c96995
Sebastian Olafsson <sebasto@stud.ntnu.no>
2018-06-13 09:52:36 +0200

    added total number of circuits and customers to index. resolves #24

--- navtelemator/views/index.py
+++ navtelemator/views/index.py
@@ -3,6 +3,12 @@ from navtelemator import services
 
 
 def render_index(request):
+    total_circuits = services.get_circuit_amount()
+    total_customers = services.get_customer_amount()
     return render(request,
                   'telemator/index_info.html',
+                  {
+                      'total_circuits': total_circuits,
+                      'total_customers': total_customers
+                  }
                   )
