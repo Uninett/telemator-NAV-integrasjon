219f48ae8c3bdea168c7bbb17d92b33636b7708c
Sebastian Olafsson <sebasto@stud.ntnu.no>
2018-06-14 15:57:22 +0200

    refactored api functionality to /api and renamed to customer_circuits

--- navtelemator/api/customer_circuits.py
+++ navtelemator/api/customer_circuits.py
@@ -3,16 +3,16 @@ from navtelemator import services
 from django.http import JsonResponse, Http404
 
 
-def render_customer_capacity(request, customerid):
+def render_customer_circuits(request, customerid):
     try:
         customer = services.get_customer_by_id(customerid)
-        circuit_capacity = {}
+        customer_circuits = {}
         circuits = []
         for circuit in customer.circuits:
             circuits.append(circuit.circuit)
         for circuit in circuits:
-            circuit_capacity[circuit.Circuit] = circuit.Speed
-        return JsonResponse(circuit_capacity)
+            customer_circuits[circuit.Circuit] = circuit.Speed
+        return JsonResponse(customer_circuits)
     except:
         raise Http404
 
--- navtelemator/urls.py
+++ navtelemator/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
-from views import index, circuit, owner, cable, customer, customer_capacity, netbox
+from views import index, circuit, owner, cable, customer, netbox
+from navtelemator.api import customer_circuits
 
 urlpatterns = patterns('',
                        url(r'^$', index.render_index, name='index-list'),
@@ -8,7 +9,7 @@ urlpatterns = patterns('',
                        url(r'^circuits/', circuit.render_circuits, name='circuit-list'),
                        url(r'^customers/(?P<customerid>.+)', customer.render_customer, name='customer-info'),
                        url(r'^customers/', customer.render_customer_list, name='customer-list'),
-                       url(r'^api/1.0/customer_circuits/(?P<customerid>.+)', customer_capacity.render_customer_capacity, name='customer-capacity'),
+                       url(r'^api/1.0/customer_circuits/(?P<customerid>.+)', customer_circuits.render_customer_circuits, name='customer-capacity'),
                        url(r'^ipdevinfo/(?P<netbox_sysname>.+)/circuits/', circuit.netbox_circuits,
                            name='netbox-info-circuits'),
                        url(r'^ipdevinfo/(?P<netbox_sysname>.+)', netbox.render_netbox,
