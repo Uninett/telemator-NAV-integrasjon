c38a67a70f69d67003721151c0b902f64cf90388
Sebastian Olafsson <sebasto@stud.ntnu.no>
2018-06-29 15:04:03 +0200

    changed fields in api

--- navtelemator/api/customer_circuits.py
+++ navtelemator/api/customer_circuits.py
@@ -6,13 +6,17 @@ from django.http import JsonResponse, Http404
 def render_customer_circuits(request, customerid):
     try:
         customer = services.get_customer_by_id(customerid)
-        customer_circuits = {}
+        return_list = []
         circuits = []
         for circuit in customer.circuits:
             circuits.append(circuit.circuit)
         for circuit in circuits:
-            customer_circuits[circuit.Circuit] = circuit.Speed
-        response = JsonResponse(customer_circuits)
+            customer_circuits = {}
+            customer_circuits["Circuit"] = circuit.Circuit
+            customer_circuits["Speed"] = circuit.Speed
+            customer_circuits["Reference"] = circuit.Reference
+            return_list.append(customer_circuits)
+        response = JsonResponse(return_list, safe=False)
         response["Access-Control-Allow-Origin"] = "*"
         response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
         response["Access-Control-Max-Age"] = "1000"
